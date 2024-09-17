bdc89544fea96163f8768604f718e2ba631683eb
noura <noura.tu19@gmail.com>
2024-09-17 07:15:44 +0300

    add home page

--- main.js
+++ main.js
@@ -6,35 +6,57 @@ document.addEventListener('DOMContentLoaded', function() {
     const passwordError = document.getElementById('passwordError');
 
     loginForm.addEventListener('submit', function(event) {
-        event.preventDefault();  
+        event.preventDefault();
 
-      
+       
         emailError.textContent = '';
         passwordError.textContent = '';
 
-        
+         
         if (!emailInput.value.trim()) {
             emailError.textContent = 'Email is required';
         } else if (!isValidEmail(emailInput.value)) {
             emailError.textContent = 'Invalid email format';
         }
-
-        
         if (!passwordInput.value.trim()) {
             passwordError.textContent = 'Password is required';
         }
 
-        
         if (!emailError.textContent && !passwordError.textContent) {
-             
-            console.log('Email:', emailInput.value);
-            console.log('Password:', passwordInput.value);
+            fetch('https://66e7e6b3b17821a9d9da6ff8.mockapi.io/login', {
+                method: 'POST',
+                body: JSON.stringify({
+                    email: emailInput.value,
+                    password: passwordInput.value
+                }),
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            })
+            .then(response => {
+                if (!response.ok) throw new Error('Network response was not ok.');
+                return response.json();
+            })
+            .then(data => {
+                
+                const userId = data.id;
+                
+                
+                sessionStorage.setItem('userId', userId);
+
+            
+                window.location.href = 'home.html';
+            })
+            .catch(error => {
+                console.error('There was a problem with the fetch operation:', error);
+            });
         }
     });
 
-   
     function isValidEmail(email) {
         const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
         return regex.test(email);
     }
 });
+
+ 
